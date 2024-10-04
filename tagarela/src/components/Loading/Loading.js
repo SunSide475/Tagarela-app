e40fd0d952e0174f0c03b30091b1d5d473c81d4a
Murilo <127051508+murilo587@users.noreply.github.com>
2024-10-04 12:40:45 -0300

    fix: resolve merge conflicts in image files

---
+++ tagarela/src/components/Loading/Loading.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef } from 'react';
+import { StyleSheet, View, Animated } from 'react-native';
+
+export const Loading = () => {
+    const animations = useRef([
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+        new Animated.Value(0),
+    ]).current;
+
+    useEffect(() => {
+        const animate = () => {
+            const animationList = animations.map((animation, index) => 
+                Animated.sequence([
+                    Animated.delay(index * 300), // Atraso para cada bolinha
+                    Animated.timing(animation, {
+                        toValue: -10,
+                        duration: 500,
+                        useNativeDriver: true,
+                    }),
+                    Animated.timing(animation, {
+                        toValue: 0,
+                        duration: 500,
+                        useNativeDriver: true,
+                    }),
+                ])
+            );
+
+            Animated.loop(
+                Animated.parallel(animationList)
+            ).start();
+        };
+
+        animate();
+    }, [animations]);
+
+    return (
+        <View style={styles.loadingContainer}>
+            <View style={styles.loading}>
+                {animations.map((animation, index) => (
+                    <Animated.View 
+                        key={index} 
+                        style={[styles.dots, { transform: [{ translateY: animation }] }]} 
+                    />
+                ))}
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    loadingContainer: {
+        width: "100%",
+        height: "100%",
+        justifyContent: 'center',
+        alignItems: "center"
+    },
+    loading: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    dots: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        backgroundColor: "purple",
+        marginHorizontal: 5,
+    }
+});

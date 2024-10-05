9278d7e64d47dbf680c2ba7ea6fce2cfd10303c8
Murilo <127051508+murilo587@users.noreply.github.com>
2024-10-05 13:21:39 -0300

    feat: image import utility

---
+++ tagarela/src/assets/assets.js
@@ -0,0 +1,10 @@
+const images = {
+    logo: require('./tagarela_logo.png'),
+    settings: require('./icon-settings.png'),
+    home: require('./icon-home.png'),
+    find: require('./icon-find.png'),
+    alarm: require('./icon-alarm.png'),
+    head: require('./img-head.png')
+}
+
+export default images

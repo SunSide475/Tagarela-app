ad5123c730ef8d107af3f291deea8984b1b4c908
Murilo <127051508+murilo587@users.noreply.github.com>
2024-11-12 14:17:28 -0300

    wip: first implementation of the video player on customModal component

--- src/components/CustomModal/CustomModal.js
+++ src/components/CustomModal/CustomModal.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import Menu from '../Menu/Menu';
-import { separateSyllables } from '../../utils/separateSyllables';
-import { Modal, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React from "react";
+import Menu from "../Menu/Menu";
+import { separateSyllables } from "../../utils/separateSyllables";
+import {
+  Modal,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Platform,
+  Image,
+} from "react-native";
+
+let VideoPlayer;
+if (Platform.OS !== "web") {
+  VideoPlayer = require("react-native-video").default;
+}
 
 const CustomModal = ({ isVisible, onClose, cardInfo }) => {
   if (!isVisible) return null;
 
-  const syllables = separateSyllables(cardInfo?.description || '');
+  const syllables = separateSyllables(cardInfo?.description || "");
+
+  const videoUrl = require("../../assets/videos/cute_cats.mp4");
+
+  const imageUrl =
+    "https://em-content.zobj.net/source/apple/391/dog-face_1f436.png";
 
   return (
     <Modal
@@ -18,8 +36,22 @@ const CustomModal = ({ isVisible, onClose, cardInfo }) => {
       <View style={styles.modalOverlay}>
         <View style={styles.modalContent}>
           <Text style={styles.modalTitle}>{cardInfo?.title}</Text>
-          <View style={styles.videoContainer}></View>
-          
+
+          <View style={styles.videoContainer}>
+            {Platform.OS === "web" ? (
+              <Image source={{ uri: imageUrl }} style={styles.image} />
+            ) : (
+              <VideoPlayer
+                source={videoUrl}
+                style={styles.video}
+                controls={true}
+                onBuffer={() => console.log("Buffering...")}
+                onError={(e) => console.log(e)}
+                playing={true}
+              />
+            )}
+          </View>
+
           <View style={styles.syllablesContainer}>
             {syllables.map((syllable, index) => (
               <View key={index} style={styles.syllableBox}>
@@ -27,7 +59,7 @@ const CustomModal = ({ isVisible, onClose, cardInfo }) => {
               </View>
             ))}
           </View>
-          
+
           <TouchableOpacity style={styles.closeButton} onPress={onClose}>
             <Text style={styles.closeButtonText}>Fechar</Text>
           </TouchableOpacity>
@@ -41,61 +73,71 @@ const CustomModal = ({ isVisible, onClose, cardInfo }) => {
 const styles = StyleSheet.create({
   modalOverlay: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   modalContent: {
-    width: '100%',
-    height: '75%',
-    position: 'absolute',
+    width: "100%",
+    height: "75%",
+    position: "absolute",
     padding: 20,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 10,
-    alignItems: 'center',
-    bottom: 0
+    alignItems: "center",
+    bottom: 0,
   },
   modalTitle: {
     color: "#4F4F4F",
     fontSize: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
+  videoContainer: {
+    width: "100%",
+    height: 200,
+    backgroundColor: "gray",
+    marginBottom: 20,
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
   syllablesContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap', 
-    justifyContent: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
     marginVertical: 20,
   },
   syllableBox: {
     width: 50,
     height: 50,
-    backgroundColor: '#FF9900', 
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#FF9900",
+    justifyContent: "center",
+    alignItems: "center",
     margin: 5,
     borderRadius: 5,
   },
   syllableText: {
     fontSize: 15,
-    fontWeight: 'medium',
-    color: '#FFFFFF',  
+    fontWeight: "medium",
+    color: "#FFFFFF",
   },
   closeButton: {
     paddingVertical: 10,
     paddingHorizontal: 20,
-    backgroundColor: '#3498db',
+    backgroundColor: "#3498db",
     borderRadius: 5,
   },
   closeButtonText: {
-    color: 'black',
+    color: "black",
     fontSize: 16,
   },
-  videoContainer: {
-    width: "70%",
-    height: "30%",
-    backgroundColor: "gray",
-  }
 });
 
 export default CustomModal;

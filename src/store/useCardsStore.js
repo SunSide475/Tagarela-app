c2b849831b0327588a44db17f0d7da856edc3e49
Murilo <127051508+murilo587@users.noreply.github.com>
2024-11-17 16:44:09 -0300

    feat: getMostViewedCards function on useCardsStore

--- src/store/useCardsStore.js
+++ src/store/useCardsStore.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const useCardsStore = create((set) => ({
   cards: [],
   recentCards: [],
+  mostViewedCards: [],
   error: null,
   loading: false,
   getAllCards: async () => {
@@ -25,7 +26,6 @@ const useCardsStore = create((set) => ({
       );
 
       if (Array.isArray(response.data.history)) {
-        console.log(response.data.history);
         set({
           recentCards: response.data.history,
           error: null,
@@ -38,6 +38,28 @@ const useCardsStore = create((set) => ({
       const errorMessage =
         error.response?.data?.error || error.message || "Erro ao carregar";
       set({ error: errorMessage, recentCards: [], loading: false });
+    }
+  },
+  getMostViewedCards: async (userId) => {
+    set({ loading: true, error: null });
+    try {
+      const response = await axios.get(
+        `http://10.0.2.2:4000/user/${userId}/more_viewed`
+      );
+
+      if (Array.isArray(response.data.items)) {
+        set({
+          mostViewedCards: response.data.items,
+          error: null,
+          loading: false,
+        });
+      } else {
+        set({ mostViewedCards: [], error: null, loading: false });
+      }
+    } catch (error) {
+      const errorMessage =
+        error.response?.data?.error || error.message || "Erro ao carregar";
+      set({ error: errorMessage, mostViewedCards: [], loading: false });
     }
   },
 }));
